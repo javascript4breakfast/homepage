480a650b2bdc73ea5c659244694ed0882080d426
joortiz <joortiz@adobe.com>
2019-08-24 17:23:40 -0700

    update to build

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-['jonathan', 'maverick', 'charlotte'].forEach(alert);
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Application = () => {
+    return (
+        <div>hello maverick!</div>
+    );
+};
+
+ReactDOM.render(<Application />, document.querySelector('#root'));
